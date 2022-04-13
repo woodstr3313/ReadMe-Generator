ee7fd3fa76e8e9dc5b1abf564ce689be07fb64ba
Travis Woods <woodstr3313@gmail.com>
2022-04-13 15:52:48 -0400

    updated generateMarkdown file

--- Develop/newREADME.md
+++ Develop/newREADME.md
@@ -1,8 +1,8 @@
-# hi
+# Mine
   # <Your-Project-Title>
 
 ## Description
-
+  Its fun
 Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
 - What was your motivation?
@@ -16,15 +16,19 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 - [Installation](#installation)
 - [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
 - [Credits](#credits)
 - [License](#license)
+- [Tests](#test)
 
 ## Installation
+  npm i
 
 What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
 ## Usage
-
+  its cool
 Provide instructions and examples for use. Include screenshots as needed.
 
 To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
@@ -40,6 +44,7 @@ If you followed tutorials, include links to those here as well.
 
 ## License
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]
+(https://opensource.org/licenses/Apache-2.0)
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
 ---
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,49 +6,49 @@ function renderLicenseBadge(license) {
     return '';
    
     case 'Apache 2.0':
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
 
     case 'Boost Software License 1.0':
-      return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]';
+      return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
 
     case 'BSD 3-Clause License':
-      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]';
+      return '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
 
     case 'BSD 2-Clause License':
-      return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]';
+      return '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)';
 
     case 'Eclipse Public License 1.0':
-      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]';
+      return '![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)';
 
     case 'GNU GPL v3':
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
 
     case 'IBM Public License Version 1.0':
-      return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]';
+      return '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)';
 
     case 'ISC License (ISC)':
-      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]';
+      return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
 
     case 'The MIT License':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
 
     case 'Mozilla Public License 2.0':
-      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]';
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)';
 
     case 'The Perl License':
-      return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]';
+      return '![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)';
   
     case 'SIL Open Font License 1.1':
-      return '[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)]';
+      return '![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)';
 
     case 'The Unlicense':
-      return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]';
+      return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
 
     case 'The Do What the Fuck You Want to Public License':
-      return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]';
+      return '![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)';
 
     case 'The zlib/libpng License':
-      return '[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)]';
+      return '![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)';
 
 }}
 
@@ -60,49 +60,49 @@ function renderLicenseLink(license) {
     return '';
     
     case 'Apache 2.0':
-      return '(https://opensource.org/licenses/Apache-2.0)';
+      return 'https://opensource.org/licenses/Apache-2.0';
  
     case 'Boost Software License 1.0':
-      return '(https://www.boost.org/LICENSE_1_0.txt)';
+      return 'https://www.boost.org/LICENSE_1_0.txt';
  
     case 'BSD 3-Clause License':
-      return '(https://opensource.org/licenses/BSD-3-Clause)';
+      return 'https://opensource.org/licenses/BSD-3-Clause';
  
     case 'BSD 2-Clause License':
-      return '(https://opensource.org/licenses/BSD-2-Clause)';
+      return 'https://opensource.org/licenses/BSD-2-Clause';
  
     case 'Eclipse Public License 1.0':
-      return '(https://opensource.org/licenses/EPL-1.0)';
+      return 'https://opensource.org/licenses/EPL-1.0';
  
     case 'GNU GPL v3':
-      return '(https://www.gnu.org/licenses/gpl-3.0)';
+      return 'https://www.gnu.org/licenses/gpl-3.0';
  
     case 'IBM Public License Version 1.0':
-      return '(https://opensource.org/licenses/IPL-1.0)';
+      return 'https://opensource.org/licenses/IPL-1.0';
  
     case 'ISC License (ISC)':
-      return '(https://opensource.org/licenses/ISC)';
+      return 'https://opensource.org/licenses/ISC';
  
     case 'The MIT License':
-      return '(https://opensource.org/licenses/MIT)';
+      return 'https://opensource.org/licenses/MIT';
  
     case 'Mozilla Public License 2.0':
-      return '(https://opensource.org/licenses/MPL-2.0)';
+      return 'https://opensource.org/licenses/MPL-2.0';
  
     case 'The Perl License':
-      return '(https://opensource.org/licenses/Artistic-2.0)';
+      return 'https://opensource.org/licenses/Artistic-2.0';
    
     case 'SIL Open Font License 1.1':
-      return '(https://opensource.org/licenses/OFL-1.1)';
+      return 'https://opensource.org/licenses/OFL-1.1';
  
     case 'The Unlicense':
-      return '(http://unlicense.org/)';
+      return 'http://unlicense.org/';
  
     case 'The Do What the Fuck You Want to Public License':
-      return '(http://www.wtfpl.net/about/)';
+      return 'http://www.wtfpl.net/about/';
  
     case 'The zlib/libpng License':
-      return '(https://opensource.org/licenses/Zlib)';
+      return 'https://opensource.org/licenses/Zlib';
  
  
 }}
@@ -116,16 +116,9 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  # <Your-Project-Title>
 
 ## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+  ${data.description}
 
 ## Table of Contents (Optional)
 
@@ -133,19 +126,18 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 - [Installation](#installation)
 - [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
 - [Credits](#credits)
 - [License](#license)
+- [Tests](#test)
 
 ## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+  ${data.dependencies}
 
 ## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
+  ${data.user}
+Include screenshots as needed.
 
 ## Credits
 
@@ -156,9 +148,8 @@ If you used any third-party assets that require attribution, list the creators w
 If you followed tutorials, include links to those here as well.
 
 ## License
-${renderLicenseBadge(data.license)}
-${renderLicenseLink(data.license)}
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+[${renderLicenseBadge(data.license)}]
+(${renderLicenseLink(data.license)})
 
 ---
 
@@ -172,15 +163,11 @@ Badges aren't necessary, per se, but they demonstrate street cred. Badges let ot
 
 ## Features
 
-If your project has a lot of features, list them here.
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
 `;
 }
