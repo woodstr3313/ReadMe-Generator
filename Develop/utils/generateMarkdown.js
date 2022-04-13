417071863656319bfe748751f0560e078f291ca8
Travis Woods <woodstr3313@gmail.com>
2022-04-13 19:30:03 -0400

    added email and github to markdown file. Fixed some duplicate text in table of contents.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -127,9 +127,8 @@ If your README is long, add a table of contents to make it easy for users to fin
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
-- [Contributing](#contributing)
+- [Contributing](#repo)
 - [Credits](#credits)
-- [License](#license)
 - [Tests](#test)
 
 ## Installation
@@ -142,6 +141,9 @@ Include screenshots as needed.
 ## Credits
 
 List your collaborators, if any, with links to their GitHub profiles.
+[My GitHub: ${data.gitHub}](https://github.com/${data.gitHub})
+
+My email: [${data.email}](email:${data.email})
 
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
@@ -155,20 +157,15 @@ If you followed tutorials, include links to those here as well.
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 ## Features
 
+If your project has a lot of features, list them here.
 
 ## How to Contribute
-
+  ${data.repo}
 
 ## Tests
-
+  ${data.tests}
 `;
 }
 
