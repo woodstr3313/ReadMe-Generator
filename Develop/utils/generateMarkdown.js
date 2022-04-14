d373dff493e7aacfce58758e091b21d3ef77b47c
Travis Woods <woodstr3313@gmail.com>
2022-04-13 20:26:37 -0400

    updated markdown file again and removed redundent text

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -120,9 +120,7 @@ function generateMarkdown(data) {
 ## Description
   ${data.description}
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents 
 
 - [Installation](#installation)
 - [Usage](#usage)
